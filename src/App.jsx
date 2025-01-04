19e91422870ffd1d9676331b15480042c576a5d4
arindam-02 <das.arin.ece@gmail.com>
2025-01-05 00:01:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,37 @@ function App() {
   return (
     <>
       <div>
-        <h1>This One is New frontend App</h1>
+        <h1>User Table</h1>
+        <table class="table table-dark table-striped">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Cyty</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>USA</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>RSA</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Arindam</td>
+              <td>Das</td>
+              <td>IND</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </>
   );
